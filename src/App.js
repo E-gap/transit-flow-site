25f9b200cb347a42a10295c4d39fdce91277bf82
Evhen Gaponyuk <e-gap@ukr.net>
2023-10-26 15:52:38 +0300

    navbarcomponent complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-/* import MainSection from "./components/MainSection/MainSection";
-import AdvantagesSection from "./components/AdvantagesSection/AdvantagesSection";
-import ProductSection from "./components/ProductSection/ProductSection";
-import PricingSection from "./components/PricingSection/PricingSection";
-import OrderSection from "./components/OrderSection/OrderSection";
-import ContactSection from "./components/ContactSection/ContactSection"; */
+import NavBarComponent from "./components/NavBarComponent/NavBarComponent";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <NavBarComponent />
       <Footer />
     </div>
   );
---
+++ src/components/NavBarComponent/NavBarComponent.jsx
@@ -0,0 +1,72 @@
+import css from "./NavBarComponent.module.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { SiLinkedin } from "react-icons/si";
+import { FaTwitter, FaFacebook } from "react-icons/fa";
+import { BsInstagram } from "react-icons/bs";
+
+const NavBarComponent = () => {
+  return (
+    <section className={css.navBarComponent}>
+      <Container className="d-flex align-items-center">
+        <Navbar className={css.navBar}>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home" className={`${css.navLink} navbarLink`}>
+                Home
+              </Nav.Link>
+              <div className={`${css.navDivider} vr`} />
+              <Nav.Link href="#about" className={`${css.navLink} navbarLink`}>
+                About
+              </Nav.Link>
+              <div className={`${css.navDivider} vr`} />
+              <NavDropdown
+                title="Pages"
+                id="basic-nav-dropdown"
+                className={`${css.navLink} ${css.dropdown}`}
+              >
+                <NavDropdown.Item href="#action/3.1">Page1</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Page2</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Page3</NavDropdown.Item>
+              </NavDropdown>
+              <div className={`${css.navDivider} vr`} />
+              <Nav.Link href="#project" className={`${css.navLink} navbarLink`}>
+                Project
+              </Nav.Link>
+              <div className={`${css.navDivider} vr`} />
+              <Nav.Link href="#contact" className={`${css.navLink} navbarLink`}>
+                Contact
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <ul className={css.socialLinksList}>
+          <li className={css.socialLinksItem}>
+            <a href="/" className={css.socialLink}>
+              <BsInstagram className={css.linkIcon} />
+            </a>
+          </li>
+          <li className={css.socialLinksItem}>
+            <a href="/" className={css.socialLink}>
+              <FaFacebook className={css.linkIcon} />
+            </a>
+          </li>
+          <li className={css.socialLinksItem}>
+            <a href="/" className={css.socialLink}>
+              <FaTwitter className={css.linkIcon} />
+            </a>
+          </li>
+          <li className={css.socialLinksItem}>
+            <a href="/" className={css.socialLink}>
+              <SiLinkedin className={css.linkIcon} />
+            </a>
+          </li>
+        </ul>
+      </Container>
+    </section>
+  );
+};
+
+export default NavBarComponent;
