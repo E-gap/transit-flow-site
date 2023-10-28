968f0ef08d077347eeac4ce64615c4c30adf67b3
Evhen Gaponyuk <e-gap@ukr.net>
2023-10-28 15:35:10 +0300

    home contact complete

---
+++ src/components/FormContact/FormContact.jsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import css from "./FormContact.module.css";
+
+const FormContact = () => {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
+  return (
+    <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      <Row className="mb-4">
+        <Form.Group
+          as={Col}
+          xs="6"
+          controlId="validationCustom01"
+          className={css.formColumn}
+        >
+          <Form.Control
+            required
+            type="text"
+            placeholder="Your name*"
+            className={css.formControl}
+          />
+        </Form.Group>
+        <Form.Group
+          as={Col}
+          xs="6"
+          controlId="validationCustom02"
+          className={css.formColumn}
+        >
+          <Form.Control
+            required
+            type="email"
+            placeholder="Email*"
+            className={css.formControl}
+          />
+        </Form.Group>
+      </Row>
+      <Row className="mb-4">
+        <Form.Group
+          as={Col}
+          xs="6"
+          controlId="validationCustom04"
+          className={css.formColumn}
+        >
+          <Form.Control
+            type="phone"
+            placeholder="Phone number*"
+            required
+            className={css.formControl}
+          />
+        </Form.Group>
+        <Form.Group
+          as={Col}
+          xs="6"
+          controlId="validationCustom03"
+          className={css.formColumn}
+        >
+          <Form.Control
+            type="text"
+            placeholder="City*"
+            required
+            className={css.formControl}
+          />
+        </Form.Group>
+      </Row>
+      <Row className="mb-4">
+        <Form.Group as={Col} xs="12" className={css.formColumn}>
+          <Form.Control
+            as="textarea"
+            placeholder="Your message"
+            className={`${css.formControl} ${css.textArea}`}
+          />
+        </Form.Group>
+      </Row>
+      <Row>
+        <Col xs="4" className={css.formColumn}>
+          <Button type="submit" className={css.submitButton}>
+            Submit message
+          </Button>
+        </Col>
+      </Row>
+    </Form>
+  );
+};
+
+export default FormContact;
--- src/components/HomeContact/HomeContact.jsx
+++ src/components/HomeContact/HomeContact.jsx
@@ -1,10 +1,7 @@
 import css from "./HomeContact.module.css";
 import Container from "react-bootstrap/Container";
 import LittleText from "../LittleText/LittleText";
-import member_1 from "../../images/homeTeam/member_1.jpg";
-import member_2 from "../../images/homeTeam/member_2.jpg";
-import member_3 from "../../images/homeTeam/member_3.jpg";
-import { BsTwitter, BsFacebook, BsInstagram, BsLinkedin } from "react-icons/bs";
+import FormContact from "../FormContact/FormContact";
 
 const HomeContact = () => {
   return (
@@ -12,11 +9,46 @@ const HomeContact = () => {
       <Container>
         <div className={css.allPartSection}>
           <LittleText text="Contact" view="dark" />
-          <h2 className={css.homeContact_headline}>Get in touch with us</h2>
-          <p>
-            Leverage agile frameworks to provide a robust synopsis for strategy
-            foster collaborative thinking to further the overall value.
-          </p>
+          <div className={css.grid}>
+            <div className={css.grid_first_column}>
+              <h2 className={css.homeContact_headline}>Get in touch with us</h2>
+              <p className={css.homeContact_text}>
+                Leverage agile frameworks to provide a robust synopsis for
+                strategy foster collaborative thinking to further the overall
+                value.
+              </p>
+              <ul className={css.contactList}>
+                <li className={css.contactItem}>
+                  <a
+                    href="mailto:contact@logistics.com"
+                    className={`${css.contactInfo} ${css.contactIconEmail}`}
+                  >
+                    Email <br />
+                    contact@logistics.com
+                  </a>
+                </li>
+                <li className={css.contactItem}>
+                  <a
+                    href="tel:00112365489"
+                    className={`${css.contactInfo} ${css.contactIconPhone}`}
+                  >
+                    Call Us <br />
+                    (00) 112 365 489
+                  </a>
+                </li>
+                <li className={css.contactItem}>
+                  <p className={`${css.contactInfo} ${css.contactIconTime}`}>
+                    Mon - Sat 9.00 - 18.00 <br />
+                    Sunday Closed
+                  </p>
+                </li>
+              </ul>
+            </div>
+            <div className={css.form}>
+              <FormContact />
+            </div>
+          </div>
+          <div className={css.homeContact_bottom_background}></div>
         </div>
       </Container>
     </section>
