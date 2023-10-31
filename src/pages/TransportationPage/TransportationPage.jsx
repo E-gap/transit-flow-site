4fc27e69623c022e87a25f2cee52399b8a05b7e7
Evhen Gaponyuk <e-gap@ukr.net>
2023-10-31 16:58:11 +0200

    transportation page complete

--- src/pages/TransportationPage/TransportationPage.jsx
+++ src/pages/TransportationPage/TransportationPage.jsx
@@ -2,6 +2,11 @@ import TransportationMain from "../../components/TransportationMain/Transportati
 import Container from "react-bootstrap/Container";
 import css from "./TransportationPage.module.css";
 import project_details_photo from "../../images/transportationPage/project_details_photo.jpg";
+import storage_photo from "../../images/transportationPage/storage_photo.jpg";
+import photo_1 from "../../images/galleryList/photo_1.jpg";
+import photo_2 from "../../images/galleryList/photo_2.jpg";
+import photo_3 from "../../images/galleryList/photo_3.jpg";
+import overlay from "../../images/galleryList/overlay.png";
 
 const TransportationPage = () => {
   return (
@@ -47,6 +52,103 @@ const TransportationPage = () => {
                     Tags <span>:</span>
                   </p>
                   <p>Life Style Travel , Best Delivery</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </Container>
+      </section>
+      <section className={`${css.projects} ${css.section}`}>
+        <Container>
+          <div className={css.projects_allPartSection}>
+            <div>
+              <h2>Wharehouse Transportation</h2>
+              <p className={css.projects_text}>
+                Duis semper lacus scelerisque, aliquam leo quis, porttitor leo.
+                Etiam lobortis dapibus libero vel porttitor. Nulla tempor elit
+                nec feugiat tempus.Phasellus at quam id elit hendrerit semper
+                feugiat id nunc. Morbi quis justo velit. Duis semper lacus
+                scelerisque, aliquam leo quis, porttitor leo. Fusce lectus ex,
+                pretium efficitur suscipit sed, faucibus vel elit Integer
+                adipiscing erat eget risus sollicitudin pellentesque non erat.
+                Maecenas nibh dolor malesuada sagittis accumsan ipsum.
+                Pellentesque ultrices ultrices sapien, nec tincidunt nunc
+                posuere.
+              </p>
+              <ul className={css.projects_optionList}>
+                <li className={css.projects_optionItem}>
+                  National Customer Service Center – 24 hours a day.
+                </li>
+                <li className={css.projects_optionItem}>
+                  Online shipping navigator lets you quote, book, and track
+                  shipments
+                </li>
+                <li className={css.projects_optionItem}>
+                  Your Logistics Needs Are At The Heart Of Our Business
+                </li>
+                <li className={css.projects_optionItem}>
+                  Very careful handling of valuable goods
+                </li>
+                <li className={css.projects_optionItem}>
+                  Time saving and convenient transportation services in your
+                  area
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2>Our Project Transportation</h2>
+              <p className={css.projects_text}>
+                Duis semper lacus scelerisque, aliquam leo quis, porttitor leo.
+                Etiam lobortis dapibus libero vel porttitor. Nulla tempor elit
+                nec feugiat tempus.Phasellus at quam id elit hendrerit semper
+                feugiat id nunc. Morbi quis justo velit. Duis semper lacus
+                scelerisque, aliquam leo quis, porttitor leo. Fusce lectus ex,
+                pretium efficitur suscipit sed, faucibus vel elit Integer
+                adipiscing erat eget risus sollicitudin pellentesque non erat.
+                Maecenas nibh dolor malesuada sagittis accumsan ipsum.
+                Pellentesque ultrices ultrices sapien, nec tincidunt nunc
+                posuere.
+              </p>
+              <img src={storage_photo} alt="storage" width="100%" />
+            </div>
+            <div>
+              <h2>Related Project</h2>
+              <ul className={css.galleryList}>
+                <li className={css.galleryItem}>
+                  <a href="/" className={css.galleryItem_photoLink}>
+                    <img
+                      src={photo_1}
+                      alt="gallery appearance"
+                      className={css.block}
+                    />
+                    <div className={css.photoLink_overlay}>
+                      <img src={overlay} alt="button" />
+                    </div>
+                  </a>
+                </li>
+                <li className={css.galleryItem}>
+                  <a href="/" className={css.galleryItem_photoLink}>
+                    <img
+                      src={photo_2}
+                      alt="gallery appearance"
+                      className={css.block}
+                    />
+                    <div className={css.photoLink_overlay}>
+                      <img src={overlay} alt="button" />
+                    </div>
+                  </a>
+                </li>
+                <li className={css.galleryItem}>
+                  <a href="/" className={css.galleryItem_photoLink}>
+                    <img
+                      src={photo_3}
+                      alt="gallery appearance"
+                      className={css.block}
+                    />
+                    <div className={css.photoLink_overlay}>
+                      <img src={overlay} alt="button" />
+                    </div>
+                  </a>
                 </li>
               </ul>
             </div>
